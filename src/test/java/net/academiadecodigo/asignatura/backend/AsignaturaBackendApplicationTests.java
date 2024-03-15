783f7bc71aa5cc337767fbd12bdb6f2c9f1c4ec1
franklinrony <franklinrony2@gmail.com>
2024-03-15 16:52:30 -0600

    Agregadas dependencias iniciales

---
+++ src/test/java/net/academiadecodigo/asignatura/backend/AsignaturaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.academiadecodigo.asignatura.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsignaturaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
